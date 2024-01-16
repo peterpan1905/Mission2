60f15dccb2e0c797dc3986cd7beb5961c81b3a68
peterpan1905 <mrpiercewalker@gmail.com>
2024-01-15 18:31:40 -0700

    Commented all code

--- Dice.cs
+++ Dice.cs
@@ -10,10 +10,13 @@ namespace Mission2
     {
         public void Roll(int rolls)
         {
+            // Instance of the Random class to be used when rolling the dice
             Random random = new Random();
 
+            // Array to hold 11 values (2-12)
             int[] results = new int[11];
 
+            // Roll two die as many times as the user input. Generate random numbers. Add those numbers and save the result to the results array
             for (int i =0; i < rolls; i++)
             {
                 int die1 = random.Next(1, 7);
@@ -21,18 +24,22 @@ namespace Mission2
 
                 int sum = die1 + die2;
 
+                // Add one to the corresponding position of the array (If a two was rolled it will be stored in position 0, because 2 - 2 = 0)
                 results[sum - 2]++;
             }
 
             System.Console.WriteLine($"\nDICE ROLLING SIMULATION RESULTS\nEach \"*\" represents 1% of the total number of rolls.\nTotal number of rolls = {rolls}\n");
+            // Calculate the percentage that each number was rolled and output that to the console
             for (int i = 0; i < results.Length; i++)
             {
+                // An array to hold a string of asterisks equal to the percentage each number was rolled
                 string[] asterisks = new string[11];
 
                 // Calculate percentage of rolls each number had
                 float percentage = (float)results[i] / rolls * 100;
                 int roundedPercentage = (int)System.Math.Round(percentage, 0);
 
+                // Create a string of asterisks to be saved to the asterisks array to be printed out in place of the percentage number
                 for (int j = 0; j < roundedPercentage; j++)
                 {
                     asterisks[i] = asterisks[i] + "*";
--- Program.cs
+++ Program.cs
@@ -4,13 +4,16 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        // Create instance of Dice class found in Dice.cs
         Dice dice = new Dice();
 
+        // Create string and integer variables to convert user input to integer
         string strRolls;
         int intRolls;
 
         System.Console.WriteLine("Welcome to the dice throwing simulator!\n\nHow many dice rolls would you like to simulate?");
         strRolls = System.Console.ReadLine();
+        // Convert user input number to integer
         intRolls = int.Parse(strRolls);
 
         dice.Roll(intRolls);
