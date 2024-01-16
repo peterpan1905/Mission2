7479438dad182d153b32994277e56c8a6349ecc4
peterpan1905 <mrpiercewalker@gmail.com>
2024-01-15 18:22:15 -0700

    Initial commit

---
+++ Dice.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class Dice
+    {
+        public void Roll(int rolls)
+        {
+            Random random = new Random();
+
+            int[] results = new int[11];
+
+            for (int i =0; i < rolls; i++)
+            {
+                int die1 = random.Next(1, 7);
+                int die2 = random.Next(1, 7);
+
+                int sum = die1 + die2;
+
+                results[sum - 2]++;
+            }
+
+            System.Console.WriteLine($"\nDICE ROLLING SIMULATION RESULTS\nEach \"*\" represents 1% of the total number of rolls.\nTotal number of rolls = {rolls}\n");
+            for (int i = 0; i < results.Length; i++)
+            {
+                string[] asterisks = new string[11];
+
+                // Calculate percentage of rolls each number had
+                float percentage = (float)results[i] / rolls * 100;
+                int roundedPercentage = (int)System.Math.Round(percentage, 0);
+
+                for (int j = 0; j < roundedPercentage; j++)
+                {
+                    asterisks[i] = asterisks[i] + "*";
+                }
+                System.Console.WriteLine($"{i + 2}: {asterisks[i]}");
+            }
+            System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Mission2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Dice dice = new Dice();
+
+        string strRolls;
+        int intRolls;
+
+        System.Console.WriteLine("Welcome to the dice throwing simulator!\n\nHow many dice rolls would you like to simulate?");
+        strRolls = System.Console.ReadLine();
+        intRolls = int.Parse(strRolls);
+
+        dice.Roll(intRolls);
+    }
+}
